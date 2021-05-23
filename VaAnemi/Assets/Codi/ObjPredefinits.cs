8555ee764f53ffa160f94484b3b1368b14f3a046
RutgiFree <roger7a7@gmail.com>
2021-05-23 18:42:19 +0200

    seguim igual

--- VaAnemi/Assets/Codi/ObjPredefinits.cs
+++ VaAnemi/Assets/Codi/ObjPredefinits.cs
@@ -15,17 +15,18 @@ public class ObjPredefinits : MonoBehaviour
     {
        for (int i = 0; i < transform.childCount; i++){
             var child = transform.GetChild(i).GetComponent<Objectiu>();
-            
+
             if (child != null && child.getMyGo() != null)
             {
                 ia.setMemoryObj(child.getMyGo().name, child.getMyGo());
             }
             else
             {
-                Debug.LogError("ERROR - NULLS PER EL MITX");
+                if (transform.GetChild(i) == null) Debug.LogError("ERROR - NULLS PER EL MITX EN ELS FILLS");
+                else Debug.LogError("ERROR - NULLS PER EL MITX EN ELS VOMPONENTS DELS FILLS");
             }
 
-            Destroy(transform.GetChild(i).gameObject);//destriu el objecte no desitjat
+            //Destroy(transform.GetChild(i).gameObject);//destriu el objecte no desitjat
         }
     }
 }
