8eee2db6a19d4ed3a43c827e7f61c467ea483386
Roger <roger7a7@gmail.com>
2021-09-11 16:59:32 +0200

    Moviments amb animacio v2 (caminar, corre & idle)
    La animacio es la amteixa en tot, pero al forma de "intercalarla" i la relacio directa amb la velocitat del personatge estan moolt millor implementades. Nomes seria retocar i mrar com fer que a que "freni" o "corri menys" depenen de la pendent en la que es trove i que faci els salts (falten les animacions, actualment ns com interpolarles es tindrar que provar i mirar info per alli)
    :)

--- VaAnemi/Assets/Codi/MovePlayer.cs
+++ VaAnemi/Assets/Codi/MovePlayer.cs
@@ -9,43 +9,80 @@ public class MovePlayer : MonoBehaviour
     public Camera cam;
     public Transform groundCheck;
     public LayerMask groundMask;
-    
-    public float speed = Tags.VELCAMINARHUMA;
-    public float girSmoothTemps = 0.1f;
+
+    public float speed, acceleracio, desacceleracio;
+    public float girSmoothTemps;
     
     float radiGroundDist = 0.5f;
     float girSmoothVelocitat;
     Vector3 velocityY;
 
+    private void Start()
+    {
+        girSmoothTemps = 0.1f;
+
+        speed = 0.0f;
+        acceleracio = 10f;
+        desacceleracio = 20f;
+    }
+
     // Update is called once per frame
     void Update()
     {
         bool isGrounded = Physics.CheckSphere(groundCheck.position, radiGroundDist, groundMask);
-
-        girSmoothVelocitat = movimentPayer(controlador, cam, speed, girSmoothTemps, girSmoothVelocitat, isGrounded);
-        velocityY = gravetatPlayer(velocityY, Tags.GRAVTERRA, controlador.height, isGrounded);
-    
-    }
-
-    float movimentPayer(CharacterController c, Camera _camRef, float _speed, float _girSmoothT, float _girSmoothV, bool _isGrounded)
-    {
+        bool playerSayRUN = Input.GetKey("left shift"); // key que indica que vol corre
+       
         float horitzonalInput = Input.GetAxisRaw("Horizontal"); // si presiona "a" i "d" a la hora es pare, lo seu seria que agafes el ultim "input"
         float verticalInput = Input.GetAxisRaw("Vertical"); // si presiona "w" i "s" a la hora es pare, lo seu seria que agafes el ultim "input"
         Vector3 direccio = new Vector3(horitzonalInput, 0f, verticalInput).normalized; //aixi quan vagi en diagonal no s'accelera
 
-        if (Input.GetKey("left shift"))
+
+        if (direccio.magnitude >= 0.1f) // ens indica que vol moures
         {
-           _speed = Tags.VELCORREHUMA;
+            if (playerSayRUN && speed != Tags.VELCORRE_HUMA) //ens indica que vol corre
+            {
+                if (speed < Tags.VELCORRE_HUMA)//accelerem per corre
+                {
+                    speed += (Time.deltaTime * acceleracio);
+                    if (speed > Tags.VELCORRE_HUMA) speed = Tags.VELCORRE_HUMA; //no ens pasem de la velocitat objectiu (per si de cas)
+                }
+            }
+            else if(speed != Tags.VELCAMINAR_HUMA)//ens indica que vol caminar
+            {
+                if (speed < Tags.VELCAMINAR_HUMA)//accelerem per caminar
+                {
+                    speed += (Time.deltaTime * acceleracio);
+                    if (speed > Tags.VELCAMINAR_HUMA) speed = Tags.VELCAMINAR_HUMA; //no ens pasem de la velocitat objectiu (per si de cas)
+                }
+                else if (speed > Tags.VELCAMINAR_HUMA) //frenem per caminar
+                {
+                    speed -= (Time.deltaTime * desacceleracio);
+                    if (speed < Tags.VELCAMINAR_HUMA) speed = Tags.VELCAMINAR_HUMA; //no ens pasem de la velocitat objectiu (per si de cas)
+                }
+            }
         }
-        else
+        else //ens indica que no vol moures
         {
-           _speed = Tags.VELCAMINARHUMA;
+            if (speed > 0.0f) //frenem per pararnos
+            {
+                speed -= (Time.deltaTime * desacceleracio);
+            }
+
+            if (speed < 0.0f) speed = 0.0f; //no ens pasem de la velocitat objectiu (per si de cas)
         }
 
-        if (direccio.magnitude >= 0.1f)
+        girSmoothVelocitat = movimentPayer(controlador, cam, speed, girSmoothTemps, girSmoothVelocitat, direccio);
+        velocityY = gravetatPlayer(velocityY, Tags.GRAVTERRA, controlador.height, isGrounded);
+
+        animator.SetFloat(Tags.VELOCITAT_ANIM, speed);//animen un cop fet tot moviment
+    }
+
+    float movimentPayer(CharacterController c, Camera _camRef, float _speed, float _girSmoothT, float _girSmoothV, Vector3 _dir)
+    {
+        if (_dir.magnitude >= 0.1f) //ens movem
         {
             //es multiplicar per canviar el tipus, i li sumem el angle de la camera aixi es mou en relacio a on mire la cemra
-            float angleDesti = Mathf.Atan2(direccio.x, direccio.z) * Mathf.Rad2Deg + _camRef.transform.eulerAngles.y;
+            float angleDesti = Mathf.Atan2(_dir.x, _dir.z) * Mathf.Rad2Deg + _camRef.transform.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angleDesti, ref _girSmoothV, _girSmoothT);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
@@ -53,38 +90,8 @@ public class MovePlayer : MonoBehaviour
             Vector3 movDir = Quaternion.Euler(0f, angleDesti, 0f) * Vector3.forward;
             c.Move(movDir.normalized * _speed * Time.deltaTime);
 
-            bool caminant = animator.GetBool(Tags.ISCAMINANT_ANIMATOR); //fent aixo guanyem eficiencia, aixi no el canviem si ja esta com volem
-            bool corrent = animator.GetBool(Tags.ISCORRENT_ANIMATOR); //fent aixo guanyem eficiencia, aixi no el canviem si ja esta com volem
-
-
-            if (_speed == Tags.VELCAMINARHUMA && !caminant) //caminem
-            {
-                animator.SetBool(Tags.ISCAMINANT_ANIMATOR, true);
-            }
-
-            if (_speed == Tags.VELCORREHUMA && !corrent) //correm
-            {
-                animator.SetBool(Tags.ISCORRENT_ANIMATOR, true);
-
-            }
-            else if(_speed != Tags.VELCORREHUMA) //no correm
-            {
-                animator.SetBool(Tags.ISCORRENT_ANIMATOR, false);
-            }
-            
-          
         }
-        else //no ens movem (no es conte el "saltar") 
-        {
-            bool caminant = animator.GetBool(Tags.ISCAMINANT_ANIMATOR); //fent aixo guanyem eficiencia, aixi no el canviem si ja esta com volem
-            bool corrent = animator.GetBool(Tags.ISCORRENT_ANIMATOR); //fent aixo guanyem eficiencia, aixi no el canviem si ja esta com volem
-
-            if (caminant)
-                animator.SetBool(Tags.ISCAMINANT_ANIMATOR, false);
-            if (corrent)
-                animator.SetBool(Tags.ISCORRENT_ANIMATOR, false);
-        }
-        
+       
         return _girSmoothV;
     }
 
--- VaAnemi/Assets/Codi/Tags.cs
+++ VaAnemi/Assets/Codi/Tags.cs
@@ -9,8 +9,9 @@ public static class Tags
     readonly static public int MAXSON = 8, MAXGANA = 10, MAXSET = 10;
     readonly static public float GRAVTERRA = -9.81f*2;
     readonly static public string PLAYER = "Player", INTERACT = "Interactuable";
-    readonly static public float VELCAMINARHUMA = 6f, VELCORREHUMA = 8f;
-    readonly static public int ISCAMINANT_ANIMATOR = Animator.StringToHash("Caminar"), ISCORRENT_ANIMATOR = Animator.StringToHash("Corre");
+    readonly static public float VELCAMINAR_HUMA = 6.5f, VELCORRE_HUMA = 8f;
+    readonly static public int VELOCITAT_ANIM = Animator.StringToHash("Velocitat");
+    
 
 
 }
